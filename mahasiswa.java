f55d434715a1122d233d1a9927db9d203b6573d6
satriajun <114549025+satriajun@users.noreply.github.com>
2022-10-22 20:38:03 +0700

    Create mahasiswa.java

---
+++ mahasiswa.java
@@ -0,0 +1,11 @@
+public class Mahasiswa {
+    String nama;
+
+    Mahasiswa(String nama){
+        this.nama = nama;
+    }
+
+    void info(){
+        System.out.println("Nama Mahasiswa \t : " + this.nama);
+    }
+}
